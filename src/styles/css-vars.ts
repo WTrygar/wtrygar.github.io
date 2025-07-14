df4f387d4c17be2d0f5d854e4e41888f494bd3f8
WTrygar <chandelier.wt@gmail.com>
2025-07-14 15:41:55 +0200

    project change

---
+++ src/styles/css-vars.ts
@@ -0,0 +1,10 @@
+export default {
+  // primary colors
+  colorPeach: "#E7816B",
+  colorBlack: "#1D1C1E",
+  colorWhite: "#FFFFFF",
+  // secondary colors
+  colorPeachLight: "#FFAD9B",
+  colorGreyDark: "#333136",
+  colorGreyLight: "#F1F3F5",
+} as const;
